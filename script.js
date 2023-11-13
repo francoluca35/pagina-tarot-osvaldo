b169d4b564e1c5e057d02481af2839c3e0f03731
unknown <francolucap1@gmail.com>
2023-11-13 02:04:02 -0300

    primer version pagina web

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navList = document.querySelector('.nav-list');
+  const menuToggle = document.getElementById('menu-toggle');
+  const navList = document.querySelector('.nav-list');
 
-    menuToggle.addEventListener('click', function () {
-        navList.classList.toggle('show');
-    });
+  menuToggle.addEventListener('click', function () {
+      navList.classList.toggle('show');
+  });
+
+  
+  const navLinks = document.querySelectorAll('.nav-list a');
+
+  navLinks.forEach(function (link) {
+      link.addEventListener('click', function () {
+          navList.classList.remove('show');
+      });
+  });
 });
 
 //desplazamiento navigation
@@ -33,6 +42,16 @@ window.onscroll = function () {
   prevScrollPos = currentScrollPos;
 };
 
+//visor pdf en la misma pagina
+function openPDF() {
+  document.getElementById('pdfViewer').src = './img/Folleto.pdf'; 
+  document.getElementById('pdfModal').style.display = 'flex';
+}
+
+function closePDF() {
+  document.getElementById('pdfModal').style.display = 'none';
+}
+
 //Grid Imagenes
 function openImage(imageUrl) {
   const modal = document.getElementById('image-modal');
@@ -49,7 +68,19 @@ function closeImageModal() {
 
 //foters subscriber
 function subscribe() {
-  // Puedes personalizar esta función para realizar acciones específicas al suscribirse
-  alert('¡Gracias por suscribirte!');
+  
+  alert('¡Gracias por suscribirte a Osvaldo Angel Tarot!');
   location.reload();
 }
+
+function subscribe() {
+  document.getElementById('overlay').style.display = 'block';
+  document.getElementById('custom-alert').style.display = 'block';
+}
+
+function closeAlert() {
+  document.getElementById('overlay').style.display = 'none';
+  document.getElementById('custom-alert').style.display = 'none';
+  location.reload();
+}
+
